be614f2fc87da2f7a8e489652060b3aa060e2ab4
Avalon McRae <avalonmcrae@gmail.com>
2016-11-01 14:18:06 -0400

    AM - implement chai-enzyme

--- src/components/page/page-spec.js
+++ src/components/page/page-spec.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {expect} from 'chai';
+import chai, {expect} from 'chai';
 import {shallow} from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
 
 import Page from './page';
 
 describe('Page component', () => {
-  it('should contain header', () => {
-   const page = shallow(<Page />);
+  it('should contain header text', () => {
+    const page = shallow(<Page />);
 
     const header = page.find('h3');
 
-    expect(header.text()).to.contain('Hello World');
+    expect(header).to.contain.text('Hello World');
   });
 });
